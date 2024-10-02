da4a25c84092d0b97a77ebfb7551c45ec05cf134
Stanislav Ovsyankin <cancion2able@gmail.com>
2024-10-02 23:02:02 +0500

    Small fixes

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -10,7 +9,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
-        screen.fill((0, 0, 0))
+            
+        screen.fill("black")
         pygame.display.flip()
 
 if __name__ == "__main__":
